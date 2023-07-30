bb8568b70e943b8016cfd860f40655adad941488
Damian <damiangildea12@gmail.com>
2023-07-30 10:19:26 +0100

    Sort by feature working

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,23 +1,30 @@
 import { useState, useEffect } from "react";
 import ArticleListCard from "./ArticleListCard";
 import { getArticles } from "./utils/utils";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import TopicSearch from "./TopicSearch";
 import Error from "./ErrorPage";
+import SortBy from "./SortBy";
 
 const ArticleList = () => {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const [selectedTopic, setSelectedTopic] = useState("");
+  const [selectedSort, setSelectedSort] = useState("created_at");
 
   const location = useLocation();
+  const navigate = useNavigate(); // Add this line to import useNavigate
 
   useEffect(() => {
     setIsLoading(true);
     setIsError(false);
 
-    getArticles()
+    const queryParams = new URLSearchParams(location.search);
+    const sortFromQuery = queryParams.get("sort_by");
+    setSelectedSort(sortFromQuery || "created_at");
+
+    getArticles(sortFromQuery)
       .then((data) => {
         setArticles(data);
         setIsLoading(false);
@@ -26,7 +33,7 @@ const ArticleList = () => {
         setIsError(true);
         setIsLoading(false);
       });
-  }, []);
+  }, [location]);
 
   useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
@@ -36,6 +43,20 @@ const ArticleList = () => {
 
   const handleTopicChange = (selectedTopic) => {
     setSelectedTopic(selectedTopic);
+
+    // Update the URL with the new selected topic
+    const queryParams = new URLSearchParams(location.search);
+    queryParams.set("topic", selectedTopic);
+    navigate({ search: queryParams.toString() }); // Use navigate instead of history.push
+  };
+
+  const handleSortChange = (selectedSort) => {
+    setSelectedSort(selectedSort);
+
+    // Update the URL with the new selected sort option
+    const queryParams = new URLSearchParams(location.search);
+    queryParams.set("sort_by", selectedSort);
+    navigate({ search: queryParams.toString() }); // Use navigate instead of history.push
   };
 
   const filteredArticles = selectedTopic
@@ -56,6 +77,7 @@ const ArticleList = () => {
       <main className="article-list">
         <h2>ARTICLE LIST</h2>
         <TopicSearch onChange={handleTopicChange} />
+        <SortBy onSortChange={handleSortChange} />
         {filteredArticles.map((article) => (
           <ArticleListCard
             key={article.article_id}
